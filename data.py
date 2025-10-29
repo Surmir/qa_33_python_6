421f58a18cf9a5ade83b2c8baa6be015c1fcfac9
Vasilii Kasatov <Vetquer@yandex.ru>
2025-10-29 19:33:28 +0300

    Added class UserOne/UserTwo

---
+++ data.py
@@ -0,0 +1,16 @@
+class UserOne():
+
+    NAME = 'Аня'
+    LAST_NAME = 'Кротова'
+    ADRESS = '2-я Радиаторская улица, 6'
+    METRO_STATION = 'Войковская'
+    PHONE_NUMBER = '+79215436789'
+
+
+class UserTwo():
+
+    NAME = 'Ваня'
+    LAST_NAME = 'Булочкин'
+    ADRESS = 'улица Доватора, 17'
+    METRO_STATION = 'Спортивная'
+    PHONE_NUMBER = '+79215673412'
