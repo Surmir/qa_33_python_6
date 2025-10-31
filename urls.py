a45011610dcedecc890f742f1cd8da9025ea80d0
Vasilii Kasatov <Vetquer@yandex.ru>
2025-10-31 16:43:20 +0300

    fixed DZEN_PAGE

--- urls.py
+++ urls.py
@@ -3,4 +3,4 @@ class Url():
     MAIN_PAGE = 'https://qa-scooter.praktikum-services.ru/'
     ORDER_PAGE = f'{MAIN_PAGE}order'
     #Дзен
-    DZEN_PAGE = 'https://dzen.ru/'
+    DZEN_PAGE = 'https://dzen.ru/?yredirect=true'
