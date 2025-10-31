121dff96ffd90f784d10a8d2ca00243ef840e642
Vasilii Kasatov <Vetquer@yandex.ru>
2025-10-31 18:06:30 +0300

    Added class TestMainPage

---
+++ tests/test_main_page.py
@@ -0,0 +1,49 @@
+from pages.main_page import MainPage
+from data import Answers
+import pytest
+import allure
+
+
+class TestMainPage():
+
+    @allure.title('Проверка перехода в "Дзен" по логотипу "Яндекс"')
+    @allure.description('При нажатии на логотип Яндекса, ' \
+    'в новом окне через редирект откроется главная страница Дзена')
+    def test_transition_to_dzen(self, driver):
+        m_page = MainPage(driver)
+
+        m_page.open_main_page()
+        m_page.click_yandex_logo()
+        
+        assert m_page.check_open_page_dzen() == True
+
+    @allure.title('Проверка перехода на страницу заказа по кнопке "Заказать" в шапке страницы')
+    def test_button_order_in_header(self, driver):
+        m_page = MainPage(driver)
+
+        m_page.open_main_page()
+        m_page.click_button_order_header()
+
+        assert m_page.check_open_order_page() == True
+
+    @allure.title('Проверка перехода на страницу заказа по кнопке "Заказать" в центре страницы')
+    def test_button_order_in_middle(self, driver):
+        m_page = MainPage(driver)
+
+        m_page.open_main_page()
+        m_page.click_button_order_middle()
+
+        assert m_page.check_open_order_page() == True
+
+    @allure.title('Проверка выпадающий список в разделе «Вопросы о важном»')
+    @allure.description('Нужно проверить: при нажатии на стрелочку, открывается соответствующий текст')
+    @pytest.mark.parametrize('key', [1, 2, 3, 4, 5, 6, 7, 8])
+    def test_text_answer_correct(self, driver_main_page, key):
+        m_page = MainPage(driver_main_page)
+        expect_text = Answers.DICTIONARY_ANSWERS[key]
+
+        m_page.click_on_question(key)
+        m_page.wait_visibility_answer(key)
+        actual_text = m_page.get_text_answer(key)
+
+        assert expect_text == actual_text
