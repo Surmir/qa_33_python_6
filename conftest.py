fadf8610bb0311c4fc3c73b67cd395d08df10d3e
Vasilii Kasatov <Vetquer@yandex.ru>
2025-10-31 17:43:50 +0300

    added fixture driver_main_page

--- conftest.py
+++ conftest.py
@@ -1,11 +1,22 @@
 import pytest
 from selenium import webdriver
+from urls import Url
 
 
 #запуск браузера
 @pytest.fixture
 def driver():
     driver = webdriver.Firefox()
+
+    yield driver
+
+    driver.quit()
+
+#запуск браузера с переходом на главную страницу сайта(для множества тестов без закрытия страницы)
+@pytest.fixture(scope='class')
+def driver_main_page():
+    driver = webdriver.Firefox()
+    driver.get(Url.MAIN_PAGE)
 
     yield driver
 
