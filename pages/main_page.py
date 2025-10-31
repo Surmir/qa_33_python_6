271e12a32aef2632a1e696e06b3f0851237d1fd1
Vasilii Kasatov <Vetquer@yandex.ru>
2025-10-31 13:51:03 +0300

    added metods wait_load_main_page/wait_visibility_answer

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,6 +9,10 @@ class MainPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
+    @allure.step('Ожидание загрузки главной страницы')
+    def wait_load_main_page(self):
+        self.wait_for_visibility_element(MPLocs.MAIN_HEADER)
+
     @allure.step('Нажимаем на логотип "Яндекс"')
     def click_yandex_logo(self):
         self.click_on_element(BPLocs.HEADER_LOGO_YANDEX)
@@ -28,6 +32,10 @@ class MainPage(BasePage):
         qestion = MPLocs.DICTIONARY_QUESTIONS[key]
         self.skroll_to_element(qestion)
         self.click_on_element(qestion)
+
+    @allure.step('Ожидание появления ответа на вопрос №{key}')
+    def wait_visibility_answer(self, key):
+        self.wait_for_visibility_element(MPLocs.DICTIONARY_ANSWERS[key], 3)
 
     @allure.step('Получаем текст ответа на вопрос№{key}')
     def get_text_answer(self, key):
