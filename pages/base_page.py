8ab54b665b7ff5b13876a3121454b3ca974250bb
Vasilii Kasatov <Vetquer@yandex.ru>
2025-11-01 17:07:50 +0300

    added deleted metod select_text

--- pages/base_page.py
+++ pages/base_page.py
@@ -50,8 +50,3 @@ class BasePage():
     @allure.step('Заполняем поле({placeholder})')
     def set_data(self, placeholder, data):
         self.driver.find_element(*placeholder).send_keys(data)
-
-    @allure.step('Выбираем "{text}" из выпадающего списка')
-    def select_text(self, element, text):
-        select = Select(self.driver.find_element(*element))
-        select.select_by_visible_text(text)
